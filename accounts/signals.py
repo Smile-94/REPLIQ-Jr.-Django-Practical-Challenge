db4a95d81531dc9b5a65c40b906490c7ec616ecd
Sazzad Hossen <mshossen75@gmail.com>
2023-04-07 16:37:13 +0600

    Added accounts signals to create profile

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# # Models
-# from accounts.models import User 
-# from accounts.models import Profile
+# Models
+from accounts.models import User 
+from accounts.models import Profile
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, *args, **kwargs):
+    instance.profile.save()
--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,11 @@ from django.urls import include
 # Apps Urls
 from accounts import urls as accounts_urls
 from employee import urls as employee_urls
+from assests import urls as assests_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('assests/v1/', include(accounts_urls)),
     path('assests/v1/', include(employee_urls)),
+    path('assests/v1/', include(assests_urls)),
 ]
